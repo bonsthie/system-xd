da060a7b29d638a580da285db03eb5e65c2fc5cd
xtrm <oss@xtrm.me>
2025-02-15 17:29:49 +0100

    🔨 chore: add todolist, simplify some more stuff
    Signed-off-by: xtrm <oss@xtrm.me>

--- src/init.zig
+++ src/init.zig
@@ -4,6 +4,8 @@ const os = std.os.linux;
 const consts = @import("consts.zig");
 const errnoToError = @import("errno.zig").errnoToError;
 
+fn noop() !void {}
+
 /// Sets-up common signal handlers.
 fn setupSignalHandlers() !void {}
 
@@ -18,20 +20,32 @@ fn disableCADSyskey() !void {
 ///
 /// This includes: `/dev`, `/proc`, `/sys`, `/run`, `/tmp`.
 fn mountKernelVirtualFileSystems() !void {
-    const paths = [_][]const u8{ "/dev", "/proc", "/sys", "/run", "/tmp", "/var" };
-    for (paths) |path| {
+    const paths = .{ "/dev", "/proc", "/sys", "/run", "/tmp", "/var" };
+    inline for (paths) |path| {
         try std.fs.cwd().makePath(path);
     }
-    _ = try errnoToError(os.mount("none", "/dev", "devtmpfs", 0, 0));
-    _ = try errnoToError(os.mount("none", "/proc", "proc", 0, 0));
-    _ = try errnoToError(os.mount("none", "/sys", "sysfs", 0, 0));
-    _ = try errnoToError(os.mount("none", "/tmp", "tmpfs", 0, 0));
-    _ = try errnoToError(os.mount("none", "/run", "tmpfs", 0, 0));
-    _ = try errnoToError(os.mount("none", "/sys/kernel/debug", "debugfs", 0, 0));
-    _ = try errnoToError(os.symlink("/run", "/var/run"));
-    //TODO: check if we need cgroups?
-    // _ = try errnoToError(os.mount("none", "/sys/fs/cgroup", "tmpfs", 0, null));
-    _ = try errnoToError(os.symlink("/proc/self/fd", "/dev/fd"));
+
+    const mounts = .{
+        .{ "none", "/dev", "devtmpfs", 0, 0 },
+        .{ "none", "/proc", "proc", 0, 0 },
+        .{ "none", "/sys", "sysfs", 0, 0 },
+        .{ "none", "/tmp", "tmpfs", 0, 0 },
+        .{ "none", "/run", "tmpfs", 0, 0 },
+        .{ "none", "/sys/kernel/debug", "debugfs", 0, 0 },
+        //TODO: check if we need cgroups?
+        // .{ "none", "/sys/fs/cgroup", "tmpfs", 0, null },
+    };
+    inline for (mounts) |mount| {
+        _ = try errnoToError(os.mount(mount[0], mount[1], mount[2], mount[3], mount[4]));
+    }
+
+    const symlinkPaths = .{
+        .{ "/run", "/var/run" },
+        .{ "/proc/self/fd", "/dev/fd" },
+    };
+    inline for (symlinkPaths) |symlink| {
+        _ = try errnoToError(os.symlink(symlink[0], symlink[1]));
+    }
 }
 
 /// The true "main" function, which is where all the init stuff happens.
@@ -42,7 +56,14 @@ pub fn cowabunga() !void {
         .{ .msg = "Setup signal handlers", .func = &setupSignalHandlers },
         .{ .msg = "Disable CAD syskey", .func = &disableCADSyskey },
         .{ .msg = "Mount kernel virtual filesystems", .func = &mountKernelVirtualFileSystems },
+        .{ .msg = "Mount fsroot as read-only & fsck", .func = &noop },
+        .{ .msg = "Mount fsroot somewhere and set root (syscall i think? busybox change_root)", .func = &noop },
+        .{ .msg = "Mount /etc/fstab", .func = &noop },
+        .{ .msg = "Open /dev/console and /dev/tty0", .func = &noop },
+        .{ .msg = "Open tty's and login?", .func = &noop },
+        .{ .msg = "Start xd.aemon", .func = &noop },
     };
+
     inline for (steps) |step| {
         log.debug("+ Running {s}", .{step.msg});
         const time = std.time.milliTimestamp();
