248aa55221d131aec3cf3a9bfc26a348b0274c66
dyn <contact@dynamicdispat.ch>
2026-08-09 02:27:45 +0200

    🔨 chore: root.zig-ify
    Signed-off-by: dyn <contact@dynamicdispat.ch>

--- src/libxd/root.zig
+++ src/libxd/root.zig
@@ -1,6 +1,6 @@
 pub const log = @import("log.zig");
 pub const consts = @import("consts.zig");
 pub const format = @import("format/root.zig");
-pub const system = @import("system/system.zig");
+pub const system = @import("system/root.zig");
 pub const service = system.service;
 pub const debug = @import("debug.zig");
