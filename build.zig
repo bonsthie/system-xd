36b8fcbd41a3e2c6d1eb12c598777eb8bb4726ae
bonsthie <barnabe.bonnet@gmail.com>
2026-08-10 22:35:34 +0200

    made a network config parser

--- build.zig
+++ build.zig
@@ -43,6 +43,11 @@ pub fn build(b: *Build) void {
         .{ .name = LIBXD_NAME, .module = libxd },
     };
 
+    const networkdImports: []const Import = &.{
+        .{ .name = LIBXD_NAME, .module = libxd },
+        .{ .name = "toml", .module = toml },
+    };
+
     const init = b.addExecutable(.{
         .name = INIT_NAME,
         .root_module = b.createModule(.{
@@ -65,14 +70,13 @@ pub fn build(b: *Build) void {
     });
     b.installArtifact(cli);
 
-
     const xd_networkd = b.addExecutable(.{
         .name = NETWORKD_NAME,
         .root_module = b.createModule(.{
             .root_source_file = b.path(NETWORKD_ROOT_FILE),
             .target = target,
             .optimize = optimize,
-            .imports = imports,
+            .imports = networkdImports,
         }),
     });
     b.installArtifact(xd_networkd);
