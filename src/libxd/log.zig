4456f3193253ef2c0e8b52a902e4029ddbd022c6
dyn <contact@dynamicdispat.ch>
2026-08-12 19:32:39 +0200

    🔨 chore: error handling + klog target
    Signed-off-by: dyn <contact@dynamicdispat.ch>

--- src/libxd/log.zig
+++ src/libxd/log.zig
@@ -3,7 +3,8 @@ const log = std.log;
 
 var log_io: std.Io.Threaded = std.Io.Threaded.init_single_threaded;
 
-var log_target: std.Io.File = std.Io.File.stderr();
+var log_err: std.Io.File = std.Io.File.stderr();
+var log_out: std.Io.File = std.Io.File.stdout();
 
 pub fn customLogFn(
     comptime level: log.Level,
@@ -14,7 +15,7 @@ pub fn customLogFn(
     const io = log_io.io();
     const prefix = std.fmt.comptimePrint("[{s}/{s}]: ", .{ @tagName(scope), @tagName(level) });
     var buffer: [256]u8 = undefined;
-    var writer = log_target.writerStreaming(io, &buffer);
+    var writer = (if (level == .warn or level == .err) log_err else log_out).writerStreaming(io, &buffer);
     writer.interface.print(prefix, .{}) catch {};
     writer.interface.print(format, args) catch {};
     writer.interface.print("\n", .{}) catch {};
--- src/libxd/system/process.zig
+++ src/libxd/system/process.zig
@@ -186,8 +186,12 @@ fn spawnPosixWithTTY(tty: TTY, exec: *const ExecPosix, config: *const Config) !o
 
         switch (tty) {
             .none => {},
-            .fd => |fd| _ = try newTTY(fd, config.mode, config.ownTty),
-            .name => |name| _ = try newTTYFromName(name.io, name.path, config.mode, config.ownTty),
+            .fd => |fd| _ = newTTY(fd, config.mode, config.ownTty) catch |err| {
+                log.err("Failed to create new TTY for process {s}: {s}", .{exec.command orelse getDefaultCommand(), @errorName(err)});
+            },
+            .name => |name| _ = newTTYFromName(name.io, name.path, config.mode, config.ownTty) catch |err| {
+                log.err("Failed to create new TTY for process {s}: {s}", .{exec.command orelse getDefaultCommand(), @errorName(err)});
+            },
         }
         // TODO set term attribute ?
 
@@ -200,7 +204,7 @@ fn spawnPosixWithTTY(tty: TTY, exec: *const ExecPosix, config: *const Config) !o
             syscall.chdir(cwd) catch {};
         }
 
-        try syscall.execve(command, args, envp);
+        syscall.execve(command, args, envp) catch {};
         os.exit(1);
     }
     return @intCast(pid);
